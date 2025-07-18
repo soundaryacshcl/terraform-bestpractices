b46c8d925e60b56b231b21f13971221d6d2cd0ef
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-18 16:59:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,8 @@
-
+ terraform {
+  backend "s3" {
+    bucket       = "devops-lambda-tfstate"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+  }
+} 

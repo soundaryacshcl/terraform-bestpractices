6d5b0ff334fe27c9706405ee1bba14d2c1ab9b4d
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-18 16:58:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,28 @@
+resource "aws_security_group" "ssh_access" {
+  name        = "ssh_access"
+  description = "Allow SSH inbound traffic"
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}

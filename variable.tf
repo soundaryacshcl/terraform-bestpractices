0f6d20ecadf70c2c421bdad628077ddf667da0b9
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-18 16:52:54 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+}

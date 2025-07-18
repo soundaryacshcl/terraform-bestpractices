cef52314a568d1ab9a48067810275e261f80affc
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-18 16:52:11 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,11 @@
 
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.ec2.id
+}
+
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.ec2.public_ip
+}
+

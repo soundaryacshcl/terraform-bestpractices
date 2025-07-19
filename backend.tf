84e09af27341cb8f944a509e81d267df4707dffa
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-19 09:00:28 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
  terraform {
   backend "s3" {
-    bucket       = "terraform-bestpractices"
-    key          = "terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
+    bucket  = "terraform-bestpractices"
+    key     = "terraform.tfstate"
+    region  = var.aws_region
+    encrypt = true
   }
 } 
